15da8f31d2f475b9c4218e6ee5f76477a6cb3e4e
Samuel Kinyanjui <sax80kinya@gmail.com>
2023-11-11 08:21:38 +0300

    action reducers
    created reducers

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Nav from './components/Nav';
+import Books from './components/Books';
+import Categories from './components/Categories';
+
+function App() {
+  return (
+    <Router>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Books />} />
+        <Route path="/categories" element={<Categories />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
